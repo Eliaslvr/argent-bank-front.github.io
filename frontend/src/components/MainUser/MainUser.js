b2da1a5ee1edd2b11f8e834c026ea7a05af53b7f
Lelievre <eliaslvr59@gmail.com>
2024-10-11 21:15:44 +0200

    first commit

--- frontend/src/components/MainUser/MainUser.js
+++ frontend/src/components/MainUser/MainUser.js
@@ -1,55 +1,110 @@
-import React from 'react';
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { userProfile, updateUserName } from "../../actions/userActions";
 
 import './MainUser.css'
 
 export const MainUser = () => {
-   const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.auth.user);
+  const userData = useSelector((state) => state.user.userData);
 
-  // const [userName, setUserName] = useState("");
-  // const [display, setDisplay] = useState(true);
+  const [display, setDisplay] = useState(true);
+  const [userName, setUserName] = useState("");
 
-  const token = useSelector((state) => state.auth.user)
-  
-  // const userData = useSelector((state) => state.user.userData);
-  // console.log(userData);
-
-  // const [newUserName, setNewUserName] = useState('');
-
-  useEffect(() => {
-    const fetchUserData = async () => {
+  const fetchUserData = async () => {
+    try {
       const response = await fetch("http://localhost:3001/api/v1/user/profile", {
-        method: "POST",
+        method: "PUT",
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`
-        }
+        },
+        body: JSON.stringify({ userName })
       });
-       const user = (await response.json()).body;
+      const user = (await response.json()).body;
+      const updatedUserName = user.userName;
+      dispatch(updateUserName(updatedUserName));
+      setDisplay(true); // Rétablir l'affichage à true après la sauvegarde
 
-       console.log('dispatching', user);
-       dispatch(userProfile(user));
+      dispatch(userProfile(user));
+    } catch (error) {
+      console.log('Erreur', error);
+    }
+  };
+
+  useEffect(() => {
+    // Récupérer les données de l'utilisateur au montage du composant
+    const getUserData = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/api/v1/user/profile", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+          }
+        });
+        const user = (await response.json()).body;
+        dispatch(userProfile(user));
+      } catch (error) {
+        console.log('Erreur', error);
+      }
     };
 
-    fetchUserData();
-  }, 
-  // [dispatch]
-)
-  
-    return (
-        <div>
-            <main className="main bg-dark">
-            <div className="header">
-              <h1>Welcome back<br/></h1>
-              <button className="edit-button">Edit Name</button>
+    getUserData();
+  }, [dispatch, token]);
+
+  return (
+    <div>
+      <main className="main bg-dark">
+        <div className="header">
+          {display ? (
+            <div> 
+              <h1>Welcome back,<br/>
+                {userData.firstName} {userData.lastName}
+              </h1>
+              <button className="edit-button" onClick={() => setDisplay(false)}>
+                Edit Name
+              </button>
             </div>
+          ) : (
+            <div>
+              <h2>Edit user info</h2>
+              <form onSubmit={(e) => { e.preventDefault(); fetchUserData(); }}>
+                <div className="edit-input">
+                  <label htmlFor="username">User name:</label>
+                  <input 
+                    type="text" 
+                    id="username" 
+                    defaultValue={userData.username} 
+                    onChange={(event) => setUserName(event.target.value)} 
+                  />
+                </div>
+                <div className="edit-input">
+                  <label htmlFor="firstName">First name:</label>
+                  <input type="text" id="firstName" defaultValue={userData.firstName} disabled={true} />
+                </div>
+                <div className="edit-input">
+                  <label htmlFor="lastName">Last name:</label>
+                  <input type="text" id="lastName" defaultValue={userData.lastName} disabled={true} />
+                </div>
+                <div className="buttons">
+                  <button className="edit-username-button-save" type="submit">
+                    Save
+                  </button>
+                  <button className="edit-username-button-cancel" onClick={() => setDisplay(true)}>
+                    Cancel
+                  </button>
+                </div>
+              </form>
+            </div>
+          )}
+                      </div>
             <h2 className="sr-only">Accounts</h2>
             <section className="account">
-              <div className="account-content-wrapper">
-                <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-                <p className="account-amount">$2,082.79</p>
+            <div className="account-content-wrapper">
+              <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+              <p className="account-amount">$2,082.79</p>
                 <p className="account-amount-description">Available Balance</p>
               </div>
               <div className="account-content-wrapper cta">
@@ -61,7 +116,7 @@ export const MainUser = () => {
                 <h3 className="account-title">Argent Bank Savings (x6712)</h3>
                 <p className="account-amount">$10,928.42</p>
                 <p className="account-amount-description">Available Balance</p>
-              </div>
+            </div>
               <div className="account-content-wrapper cta">
                 <button className="transaction-button">View transactions</button>
               </div>
@@ -70,7 +125,7 @@ export const MainUser = () => {
               <div className="account-content-wrapper">
                 <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
                 <p className="account-amount">$184.30</p>
-                <p className="account-amount-description">Current Balance</p>
+              <p className="account-amount-description">Current Balance</p>
               </div>
               <div className="account-content-wrapper cta">
                 <button className="transaction-button">View transactions</button>
@@ -78,7 +133,9 @@ export const MainUser = () => {
             </section>
           </main>
         </div>
-    );
+  );
 }
 
 export default MainUser;
+
+
--- frontend/src/components/Navbar/Narbar.js
+++ frontend/src/components/Navbar/Narbar.js
@@ -1,29 +1,28 @@
 import React from 'react';
-// import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-//import { logout } from '../../actions/authActions';
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from '../../actions/authActions';
 import Image from '../../img/argentBankLogo.png'
 import './Navbar.css'
 
 function Narbar() {
     const isConnected = useSelector((state) => state.auth.user);
-    const userName = useSelector((state) => {
-        if(state.user && state.user.userData){
-            return state.user.userData.userName;
-        }
-        return undefined;
-    });
+    const userName = useSelector((state) => state.user.userData.userName);
+    //const lastName = useSelector((state) => state.user.userData.lastName)
+    const aa = useSelector((state) => state);
+    console.log(aa);
+    
 
     console.log('username ==> navbar', userName);
     
-    //const dispatch = useDispatch();
-    // const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-    // const logoutHandle = () => {
-    //     dispatch(logout());
-    //     localStorage.clear();
-    //     navigate('/');
-    // }
+    const logoutHandle = () => {
+        dispatch(logout());
+        localStorage.clear();
+        navigate('/');
+    }
 
     return (
 			<nav className="main-nav">
@@ -33,11 +32,10 @@ function Narbar() {
 				</a>
 				{isConnected ? (
 					<div>
-                        <a href='/profile' className="main-nav-item">
-                            <i className="fa fa-user-circle"></i>
-                            <p>{userName}</p>
-                        </a>
-                        <a href='/' className="main-nav-item">
+                        <Link to='/profile' className="main-nav-item">
+                            {userName}
+                        </Link>
+                        <a href='/' className="main-nav-item" onClick={logoutHandle}>
                             Sign Out
                         </a>
                     </div>
